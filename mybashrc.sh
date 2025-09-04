56e22653a41df676f3742aa68e84b3a253dc4ead
Shashi Varun <137184492+Shashivarunreddy@users.noreply.github.com>
2025-09-04 22:53:52 +0530

    Create mybashrc.sh

---
+++ mybashrc.sh
@@ -0,0 +1,8 @@
+export PS1='\[\033[01;34m\]\W\[\033[00m\] ~ '
+
+alias s='cd /mnt/s'
+
+# Go up 3, 4, 5 directories
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
