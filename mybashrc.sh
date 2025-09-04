12f67311d9f523d80982f8ce017974a0f75cc5e9
Shashivarunreddy <shashivarun6154@gmail.com>
2025-09-04 17:50:39 +0000

    added more

--- mybashrc.sh
+++ mybashrc.sh
@@ -6,3 +6,14 @@ alias s='cd /mnt/s'
 alias .3='cd ../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../../'
+
+
+# --- Minimal Git aliases ---
+alias ga='git add .'             # stage everything
+alias gc='git commit -m'         # commit with a message
+alias gcl='git clone'            # clone a repository
+alias gi='git init'              # initialize a repo
+alias gbm='git branch -M main'   # rename/create branch as main
+alias grao='git remote add origin'  # add remote origin
+alias gp='git push origin main'     # push to origin/main
+alias gpu='git push -u origin main' # push & set upstream (first push)
