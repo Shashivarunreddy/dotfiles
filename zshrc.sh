2744b84103b425cf2257634f729f89f0bb1d3a82
Shashi Varun <137184492+Shashivarunreddy@users.noreply.github.com>
2025-09-23 17:06:24 +0530

    Update zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -1,1 +1,25 @@
+plugins=(git z sudo zsh-autosuggestions zsh-syntax-highlighting)
 
+source $ZSH/oh-my-zsh.sh
+
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+alias s='cd /mnt/s'
+alias y='cd /mnt/y'
+
+# Go up 3, 4, 5 directories
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+
+
+# --- Minimal Git aliases ---
+alias ga='git add .'             # stage everything
+alias gc='git commit -m'         # commit with a message
+alias gcl='git clone'            # clone a repository
+alias gi='git init'              # initialize a repo
+alias gbm='git branch -M main'   # rename/create branch as main
+alias grao='git remote add origin'  # add remote origin
+alias gp='git push origin main'     # push to origin/main
+alias gpu='git push -u origin main' # push & set upstream (first push)
